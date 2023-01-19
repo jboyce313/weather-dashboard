1213cf6394a2974179aafe82fbde4626d38168ed
Jacob Boyce <jboyce313@gmail.com>
2023-01-19 14:34:48 -0600

    added js to retrieve data from api

--- assets/script.js
+++ assets/script.js
@@ -3,18 +3,17 @@ var searchBtn = $("#search-btn");
 var searchDisplay = $(".search");
 
 searchBtn.on("click", function () {
-  //   var previousSearch = $("<button>");
-  //   previousSearch.text(searchBox.val());
-  //   searchDisplay.append(previousSearch);
-
-  var lat = 38.81;
-  var lon = -94.53;
+  var cityName = searchBox.val();
   var apiKey = "5447ab7f3651e92ac93b0c23f2497452";
   fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
   )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
     });
+
+  var previousSearch = $("<button>");
+  previousSearch.text(searchBox.val());
+  searchDisplay.append(previousSearch);
 });
