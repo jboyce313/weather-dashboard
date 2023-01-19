355eef23a3da1742e8cc2e0dc090d0bbb9936073
Jacob Boyce <jboyce313@gmail.com>
2023-01-19 14:19:20 -0600

    added js to retrieve data from api

--- assets/script.js
+++ assets/script.js
@@ -8,12 +8,13 @@ searchBtn.on("click", function () {
   //   searchDisplay.append(previousSearch);
 
   var lat = 38.81;
-  var lon = 94.53;
+  var lon = -94.53;
   var apiKey = "5447ab7f3651e92ac93b0c23f2497452";
-
   fetch(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
-  ).then(function (response) {
-    console.log(response);
-  });
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
 });
