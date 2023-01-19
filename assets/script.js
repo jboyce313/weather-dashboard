ff88821927cbb2ba784691a762a70913ba4cea37
Jacob Boyce <jboyce313@gmail.com>
2023-01-19 11:12:37 -0600

    added api fetch on button click

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,17 @@ var searchBtn = $("#search-btn");
 var searchDisplay = $(".search");
 
 searchBtn.on("click", function () {
-  var previousSearch = $("<button>");
-  previousSearch.text(searchBox.val());
-  searchDisplay.append(previousSearch);
+  //   var previousSearch = $("<button>");
+  //   previousSearch.text(searchBox.val());
+  //   searchDisplay.append(previousSearch);
+
+  var lat = 38.81;
+  var lon = 94.53;
+  var apiKey = "5447ab7f3651e92ac93b0c23f2497452";
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+  ).then(function (response) {
+    console.log(response);
+  });
 });
