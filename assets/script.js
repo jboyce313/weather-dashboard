6816a42eb87a86211aff4aca438e8fa9ac35426a
Jacob Boyce <jboyce313@gmail.com>
2023-01-19 14:50:06 -0600

    added error checking for api call

--- assets/script.js
+++ assets/script.js
@@ -3,17 +3,36 @@ var searchBtn = $("#search-btn");
 var searchDisplay = $(".search");
 
 searchBtn.on("click", function () {
+  if (!searchBox.val()) {
+    return;
+  }
+
+  var good = true;
   var cityName = searchBox.val();
   var apiKey = "5447ab7f3651e92ac93b0c23f2497452";
   fetch(
     `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
   )
-    .then((response) => response.json())
+    .then((response) => {
+      if (response.status === 404) {
+        alert("404 error");
+        good = false;
+      }
+      return response.json();
+    })
     .then((data) => {
-      console.log(data);
+      if (good) {
+        console.log(data);
+      }
+    })
+    .then(() => {
+      if (good) {
+        var previousSearch = $("<button>");
+        previousSearch.text(searchBox.val());
+        searchDisplay.append(previousSearch);
+      }
+    })
+    .then(() => {
+      searchBox.val("");
     });
-
-  var previousSearch = $("<button>");
-  previousSearch.text(searchBox.val());
-  searchDisplay.append(previousSearch);
 });
